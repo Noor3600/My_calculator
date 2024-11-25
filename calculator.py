b075af6cef05e23afed82d2563d5fca288289822
Noor3600 <ahmedadan557@gmail.com>
2024-11-25 18:37:48 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,24 @@
+# Simple Calculator Program
+
+# Ask the user for two numbers and an operation
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operator = input("Enter the operator (+, -, *, /): ")
+
+# Perform the calculation based on the operator
+if operator == '+':
+    result = num1 + num2
+elif operator == '-':
+    result = num1 - num2
+elif operator == '*':
+    result = num1 * num2
+elif operator == '/':
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = "Cannot divide by zero!"
+else:
+    result = "Invalid operator"
+
+# Show the result
+print(f"The result is: {result}")
